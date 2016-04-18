939400341e1193455c5e92c712a2b5d8c22b1f54
threelu <215154863@qq.com>
2016-04-19 04:33:41 +0800

    4
    设置了slidingmenu,一直出错的原因是不能用死里顶menu的 jar包

--- app/src/main/java/com/example/three/siqiyan/MainActivity.java
+++ app/src/main/java/com/example/three/siqiyan/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.three.siqiyan;
 
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+
+public class MainActivity extends SlidingFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        setBehindContentView(R.layout.left_menu);// 设置侧边栏
+        SlidingMenu slidingMenu = getSlidingMenu();// 获取侧边栏对象
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);// 设置全屏触摸
+        slidingMenu.setBehindOffset(400);// 设置预留屏幕的宽度
     }
 }
